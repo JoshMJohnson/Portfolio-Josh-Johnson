ef5bfaa1e75b9b98a6b141d8ce27d0fd9515e084
JoshMJohnson <107897805+JoshMJohnson@users.noreply.github.com>
2022-06-20 21:59:30 -0500

    Update Hello_World.js

--- JavaScript/Hello_World.js
+++ JavaScript/Hello_World.js
@@ -1,9 +1,18 @@
+/*
+ * This JavaScript program simply prints a text to stdout. 
+ * Primarily used to ensure JavaScript is working properly on a system
+ *
+ * Created By: Josh Johnson
+ */
+
+/* prints to stdout where after each statement, a new line is created */
 function hello_world_one(phrase_one, phrase_two) {
     console.log(phrase_one);
     console.log(phrase_two);
 }
 hello_world_one("Hello World in JavaScript!", " - moves to next line after phrase");
 
+/* prints to stdout where after each statement, no new line is created */
 function hello_world_two(phrase_one, phrase_two) {
     process.stdout.write(phrase_one);
     process.stdout.write(phrase_two);
