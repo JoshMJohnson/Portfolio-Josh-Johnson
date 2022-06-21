d3c7618081a503a6986a01f7a75a6fbf457f122e
JoshMJohnson <107897805+JoshMJohnson@users.noreply.github.com>
2022-06-21 17:35:06 -0500

    Update Sort_Array.c

--- C/Sort_Array.c
+++ C/Sort_Array.c
@@ -84,17 +84,15 @@ int create_array_from_file() {
 void array_selection_sort(int* arr, int n) {
     int i, j, min_idx;
  
-    // One by one move boundary of unsorted subarray
+    /* one by one move boundary of unsorted subarray */
     for (i = 0; i < n - 1; i++) {
- 
-        // Find the minimum element in unsorted array
+        /* find the minimum element in unsorted array */
         min_idx = i;
         for (j = i + 1; j < n; j++)
             if (*(arr + j) < *(arr + min_idx))
                 min_idx = j;
  
-        // Swap the found minimum element
-        // with the first element
+        /* swap the found minimum element with the first element */
         swap((arr + min_idx), (arr + i));
     }
 }
