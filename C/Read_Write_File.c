6c443e19434c47572efc130d2043a3107b46484b
JoshMJohnson <107897805+JoshMJohnson@users.noreply.github.com>
2022-06-22 14:55:45 -0500

    Uploaded C file that can read and write to and from files

---
+++ C/Read_Write_File.c
@@ -0,0 +1,43 @@
+/*
+ * This C program reads from a text file, character by character, and 
+ * writes the file contents to another file, character by character
+ * 
+ * Created By: Josh Johnson
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* file_in;
+    FILE* file_out;
+    char c;
+
+    /* tries to open existing file to read from */
+    if ((file_in = fopen("../Test_Files/Poem.txt", "r")) == NULL) {
+        printf("Could not open the existing file to read from");
+        exit(EXIT_FAILURE); /* same as exit(1) */
+    }
+
+    /* tries to open or create a file to write to */
+    if ((file_out = fopen("../Test_Files/Output_Files/Poem_Dup_From_Read_Write_File_C.txt", "w")) == NULL) {
+        printf("Could not create or open file");
+        exit(EXIT_FAILURE);
+    }
+
+    /* prints file content character by character to output stream */
+    do {
+        char c;
+    
+        c = fgetc(file_in);
+        if (!feof(file_in)) {
+            fprintf(file_out, "%c", c);
+        }
+    } while (!feof(file_in));
+
+    printf("Successfully wrote to the output file");
+
+    fclose(file_in);
+    fclose(file_out);
+    return EXIT_SUCCESS; /* Same as return 0 */
+}
