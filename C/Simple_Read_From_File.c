f9494b000e84cfe87a5403e70b8a128b74970e57
JoshMJohnson <107897805+JoshMJohnson@users.noreply.github.com>
2022-06-22 00:06:50 -0500

    Updated formatting to Simple_Read_From_File.c

--- C/Simple_Read_From_File.c
+++ C/Simple_Read_From_File.c
@@ -10,20 +10,19 @@
 
 int main() {
     FILE* file;
+    char c;
 
     /* tries to open file */
-    if ((file = fopen("../Test_Files/Basic_Text.txt", "r")) == NULL) {
+    if ((file = fopen("../Test_Files/Poem.txt", "r")) == NULL) {
         printf("Could not open the file");
-
         exit(EXIT_FAILURE); /* same as exit(1) */
     }
 
     /* prints file content character by character to output stream */
     do {
         char c;
-        
+    
         c = fgetc(file);
-
         if (!feof(file)) {
             printf("%c", c);
         }
