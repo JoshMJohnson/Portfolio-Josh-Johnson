a34b670b2547c6d4fbaeaa086393b988d7f3a737
JoshMJohnson <107897805+JoshMJohnson@users.noreply.github.com>
2022-06-22 00:11:16 -0500

    Updated formatting to Sort_Array.c

--- C/Sort_Array.c
+++ C/Sort_Array.c
@@ -177,9 +177,11 @@ void array_selection_sort(int* arr, int n) {
     for (i = 0; i < n - 1; i++) {
         /* find the minimum element in unsorted array */
         min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (*(arr + j) < *(arr + min_idx))
+        for (j = i + 1; j < n; j++) {
+            if (*(arr + j) < *(arr + min_idx)) {
                 min_idx = j;
+            }
+        }
  
         /* swap the found minimum element with the first element */
         swap((arr + min_idx), (arr + i));
