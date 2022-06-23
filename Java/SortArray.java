5737fbca1fb97650f2ccce2bebe2ae095ea852b1
JoshMJohnson <joshjohns21@gmail.com>
2022-06-23 16:20:29 -0500

    Update SortArray.java
    Added ability to read from a file and create an array of integers from the elements present in the file that was read from

--- Java/SortArray.java
+++ Java/SortArray.java
@@ -12,8 +12,11 @@
  * Created By: Josh Johnson
  */
 
- import java.util.ArrayList;
- import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.lang.Math;
 
  public class SortArray {
     public static void main (String [] args) {
@@ -21,17 +24,56 @@
 
         /* array one - size and values are given by program */
         int[] array_one = new int[20];
-        array_one = sa.create_array(array_one);
+        array_one = sa.fill_array(array_one);
         sa.print_array(array_one, 1, false, "Selection Sort");
         sa.array_selection_sort(array_one);
         sa.print_array(array_one, 1, true, "Selection Sort");
         
         /* array two - array size and values are discovered by reading a file */
-        int[] array_two; 
+        ArrayList<Integer> arr_two_list = sa.create_array_from_file();
+        int[] array_two = sa.create_array_from_list(arr_two_list);
+        sa.print_array(array_two, 2, false, "Selection Sort");
+        sa.array_selection_sort(array_two);
+        sa.print_array(array_two, 2, true, "Selection Sort");
+    }
+
+    /* reads from a file of integers and adds elements to an arraylist */
+    private ArrayList<Integer> create_array_from_file() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        try{
+            File file_in = new File("../Test_Files/Random_Integers_With_Duplicates.txt");
+            Scanner reader = new Scanner(file_in);
+
+            while (reader.hasNextInt()) {
+                int value = reader.nextInt();
+                list.add(value);
+            }
+
+            System.out.println("Successfully read from file");
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not find file");
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    /* creates an array from an arraylist */
+    private int[] create_array_from_list(ArrayList<Integer> list) {
+        int arr_size = list.size();
+        int[] arr = new int[arr_size];
+
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+
+        return arr;
     }
 
     /* fills in an initialized array with random values */
-    private int[] create_array (int[] arr) {
+    private int[] fill_array (int[] arr) {
         int value;
 
         for (int i = 0; i < arr.length; i++) {
@@ -49,7 +91,7 @@
         {
             /* find the minimum element in unsorted array */
             int min_idx = i;
-            for (int j = i+1; j < arr.length; j++)
+            for (int j = i + 1; j < arr.length; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
  
