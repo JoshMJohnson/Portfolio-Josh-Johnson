01f18772fcd3da5f23e5a4dabc3ce6bed0b7aff2
JoshMJohnson <joshjohns21@gmail.com>
2022-06-25 15:18:23 -0500

    Update SortArray.java
    Implemented the insertion sort algorithm

--- Java/SortArray.java
+++ Java/SortArray.java
@@ -4,10 +4,10 @@
  *
  * Sorting algorithms implemented
  * - Selection Sort
- * - TO DO: Bubble Sort
- * - TO DO: Merge Sort
+ * - Bubble Sort
+ * - Insertion Sort
  * - TO DO: Quick Sort
- * - TO DO: Insertion Sort
+ * - TO DO: Merge Sort
  * 
  * Created By: Josh Johnson
  */
@@ -29,31 +29,30 @@ import java.lang.Math;
         sa.print_array(array_one, 1, false, "Selection Sort");
         sa.array_selection_sort(array_one);
         sa.print_array(array_one, 1, true, "Selection Sort");
-        
-        /* selection sort - array size and values are discovered by reading a file */
-        ArrayList<Integer> arr_two_list = sa.create_array_from_file();
-        int[] array_two = sa.create_array_from_list(arr_two_list);
-        sa.print_array(array_two, 2, false, "Selection Sort");
-        sa.array_selection_sort(array_two);
-        sa.print_array(array_two, 2, true, "Selection Sort");
 
         /* bubble sort - size and values are given by program */
         int[] array_three = {2, 5, 3, 9, 21, 56, 1, 2, 7, 3, 99, 65, 21, 8};
-        sa.print_array(array_three, 3, false, "Bubble sort");
+        sa.print_array(array_three, 2, false, "Bubble Sort");
         sa.array_bubble_sort(array_three);
-        sa.print_array(array_three, 3, true, "Bubble sort");
+        sa.print_array(array_three, 2, true, "Bubble Sort");
 
-        /* TO DO: merge sort - array size and values are discovered by reading a file */
-        /* TO DO: quick sort - array size and values are discovered by reading a file */
-        /* TO DO: insertion sort - array size and values are discovered by reading a file */
+        /* insertion sort - array size and values are discovered by reading a file */
+        ArrayList<Integer> array_four_list = sa.create_array_from_file("../Test_Files/Random_Integers_No_Duplicates.txt");
+        int[] array_four = sa.create_array_from_list(array_four_list);
+        sa.print_array(array_four, 3, false, "Insertion Sort");
+        sa.array_insertion_sort(array_four);
+        sa.print_array(array_four, 3, true, "Insertion Sort");
+
+        /* TO DO: Insertion sort - array size and values are discovered by reading a file */
+        /* TO DO: Merge sort - array size and values are discovered by reading a file */
     }
 
     /* reads from a file of integers and adds elements to an arraylist */
-    private ArrayList<Integer> create_array_from_file() {
+    private ArrayList<Integer> create_array_from_file(String file_name) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
         try{
-            File file_in = new File("../Test_Files/Random_Integers_With_Duplicates.txt");
+            File file_in = new File(file_name);
             Scanner reader = new Scanner(file_in);
 
             while (reader.hasNextInt()) {
@@ -83,7 +82,7 @@ import java.lang.Math;
     }
 
     /* fills in an initialized array with random values */
-    private int[] fill_array (int[] arr) {
+    private int[] fill_array(int[] arr) {
         int value;
 
         for (int i = 0; i < arr.length; i++) {
@@ -97,13 +96,13 @@ import java.lang.Math;
     /* performs a selection sort algorithm */
     private int[] array_selection_sort(int[] arr) { 
         /* one by one move boundary of unsorted subarray */
-        for (int i = 0; i < (arr.length - 1); i++)
-        {
+        for (int i = 0; i < (arr.length - 1); i++) {
             /* find the minimum element in unsorted array */
             int min_idx = i;
-            for (int j = i + 1; j < arr.length; j++)
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
+            }
  
             /* swap the found minimum element with the first element */
             int temp = arr[min_idx];
@@ -116,14 +115,37 @@ import java.lang.Math;
 
     /* performs a bubble sort algorithm */
     private int[] array_bubble_sort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++)
-            for (int j = 0; j < arr.length - i - 1; j++)
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 /* swap arr[j+1] and arr[j] */
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
+            }
+        }
+
+        return arr;
+    }
+
+    /* performs an insertion sort algorithm */
+    private int[] array_insertion_sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+ 
+            /* 
+             * Move elements of arr[0..i-1], that are greater than key, 
+             * to one position ahead of their current position 
+             */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
 
         return arr;
     }
