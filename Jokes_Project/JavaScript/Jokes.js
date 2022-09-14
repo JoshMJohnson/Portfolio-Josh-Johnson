c2b10f7ffd61ff7e04706993381d27ac5e63aff7
JoshMJohnson <joshjohns21@gmail.com>
2022-09-14 17:36:03 -0500

    Organization update

---
+++ Jokes_Project/JavaScript/Jokes.js
@@ -0,0 +1,21 @@
+const port = 3000;
+var path = require('path');
+var fs = require('fs');
+var http = require('http');
+const express = require('express');
+const app = express();
+
+app.listen(port, () => console.log('listening at 3000'));
+app.use(express.static(__dirname + '/../../HTML/Jokes'));
+
+app.use(express.json());
+
+/* adding jokes */
+app.post('/', (request, response) => {
+    console.log('Joke Added!!');
+});
+
+/* hearing jokes */
+app.get('/', (request, response) => {
+    console.log('Jokes Requested!!');
+})
