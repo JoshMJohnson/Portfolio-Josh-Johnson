ff5c23d22eea06d6f95ce86cb12e3e38354e2c7e
JoshMJohnson <joshjohns21@gmail.com>
2022-09-16 12:47:23 -0500

    Update app.js

--- Jokes_Project/app.js
+++ Jokes_Project/app.js
@@ -4,8 +4,10 @@ const port = 3000;
 
 app.listen(port, () => console.log(`listening at ${port}`));
 
+/* provides access to all files within the specified folder */
 app.use(express.static('Public'));
 
+/* gives paths to web pages within the app */
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/Public/HTML/jokes.html');
 });
