7e2691b34c5c93feaa024d7dd52df0e08bf6ba95
JoshMJohnson <joshjohns21@gmail.com>
2022-11-09 01:25:48 -0600

    Connection to database successful
    Successfully connects to the database accessing the members table from the arrow_fan_club.db file and adds member info to new rows in the table

--- Account_Manager/Public/JavaScript/logged_in.js
+++ Account_Manager/Public/JavaScript/logged_in.js
@@ -1,1 +1,29 @@
 /* client side JavaScript for when a user logs in successfully */
+getData();
+
+/* gets a list of all the members currently in the database */
+async function getData() {
+    const response = await fetch('/members');
+    const data = await response.json();
+    const members = document.getElementById('club_members');
+    
+    /* add members from database to the web page */
+    for (let i = 0; i < data.length; i++) {        
+        /* create a row */
+        let row = members.insertRow(1);
+
+        /* identify columns of new row */
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+        let cell3 = row.insertCell(3);
+        let cell4 = row.insertCell(4);
+
+        /* fill columns of new row */        
+        cell0.textContent = `${data[i].name}`;
+        cell1.textContent = `${data[i].email}`;
+        cell2.textContent = `${data[i].dob}`;
+        cell3.textContent = `${data[i].phoneNumber}`;
+        cell4.textContent = `${data[i].status}`;
+    }
+}
--- Account_Manager/app.js
+++ Account_Manager/app.js
@@ -1,1 +1,62 @@
 /* server side JavaScript for the Account Manager project */
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.listen(port, () => console.log(`listening at ${port}`));
+
+/* provides access to all files within the specified folder */
+app.use(express.static('Public'));
+app.use(express.json({limit: '1mb'}));
+
+/* gives paths to web pages within the site */
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/Public/HTML/logged_in.html');
+});
+
+/* manage database */
+const sqlite3 = require('sqlite3').verbose();
+const database = 'arrow_fan_club.db';
+
+/* connect to database */
+const db = new sqlite3.Database(database, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connection to database was successful');
+});
+
+/* create database table to hold fan club members */ /*
+db.run (
+    `CREATE TABLE members (
+        email       STRING       PRIMARY KEY
+                                 UNIQUE
+                                 NOT NULL,
+        name        STRING       NOT NULL,
+        dob         DATE,
+        phoneNumber VARCHAR (12),
+        password    STRING,
+        avatar      INTEGER      NOT NULL,
+        status      STRING       DEFAULT user
+    );`
+); */
+
+/* retrieves the fan club members from the database */
+app.get('/members', (request, response) => {
+    const sql = `SELECT * FROM members`;
+    const sendBack = [];
+
+    db.all(
+        sql, [], (err, rows) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            
+            rows.forEach((row) => {
+                sendBack.push(row);
+            });
+                        
+            response.json(sendBack);
+    });
+});
