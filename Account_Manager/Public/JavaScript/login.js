7d5cede837920e8f11a5a2ce79c9f20ad36cf7f0
JoshMJohnson <joshjohns21@gmail.com>
2022-11-17 19:14:19 -0600

    Implemented a show password option for login screen

--- Account_Manager/Public/JavaScript/login.js
+++ Account_Manager/Public/JavaScript/login.js
@@ -1,3 +1,14 @@
 /* 
  * client side JavaScript for the login screen 
  */
+
+/* password show/hide toggle */
+function showPassword() {
+    const showPass = document.getElementById('password');    
+
+    if (showPass.type === "password") { /* if currently hidden */
+        showPass.type = "text";
+    } else { /* else currently shown */
+        showPass.type = "password";
+    }
+}
