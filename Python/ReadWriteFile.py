42bdf832933c6b1df01be5dbbeae188eaa2ecd0f
JoshMJohnson <joshjohns21@gmail.com>
2022-11-25 21:10:11 -0600

    Implemented ability to write to output file

--- Python/ReadWriteFile.py
+++ Python/ReadWriteFile.py
@@ -1,6 +1,6 @@
 # Demonstrates basic file operations in python
 #   - Reads a text file and writes content of file to stdout
-#   - TODO: Reads a text file and writes content of file into an output file
+#   - Reads a text file and writes content of file into an output file
 #
 # Created By: Josh Johnson
 
@@ -13,16 +13,39 @@ base_path = path.dirname(__file__)
 # relative path to the test file
 file_path = path.abspath(path.join(base_path, "..", "Test_Files", "basic_text.txt"))
 
-# open text file
+# open access to test file
 file1 = open(file_path, "r")
 
-# reading from variable storing access to the text file and saving its content
+# reading from variable storing access to the test file and saving its content
 data = file1.read()
 
-# closing the file access
+# closing the file access to test file
 file1.close()
 
-# printing info stored in file1 to stdout
+# printing data stored from reading file to stdout
 print(data)
 
+# *** reads a text file and writes content of file into an output file ***
+# relative path to the test file
+file_path = path.abspath(path.join(base_path, "..", "Test_Files", "poem.txt"))
 
+# open access to test file
+file1 = open(file_path, "r")
+
+# reading from variable storing access to the test file and saving its content
+data = file1.read()
+
+# closing the file access to test file
+file1.close()
+
+# relative path to the output file
+file_path = path.abspath(path.join(base_path, "Output_Files", "poem_dup_from_readwritefile.txt"))
+
+# creates output file access
+file2 = open(file_path, "w")
+
+# write content from test file to output file
+file2.write(data)
+
+# closing the file access to the output file
+file2.close()
