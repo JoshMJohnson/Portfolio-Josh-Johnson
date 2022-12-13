e2f7a88c765bb209b783651fcafaad9c2b2392d6
JoshMJohnson <joshjohns21@gmail.com>
2022-12-13 16:21:26 -0600

    Randomized who goes first in a new game

--- TicTacToe/Main.py
+++ TicTacToe/Main.py
@@ -3,24 +3,32 @@
 # Created By: Josh Johnson
 
 from tkinter import * # used for GUI Interface
+import tkinter.messagebox # pop-up message box
+import random # random number generation
 
+window = -1 # gui window
 window_width = -1 # width of the gui window
+
+# game details
 stop_game = False
+begin_game = False
+current_player_display = ''
 
 # player details
 player1_symbol = 'X'
 player2_symbol = 'O'
 player1_name = 'Player One'
 player2_name = 'Player Two'
-current_player = player1_name # contains the name of the player who has to make the next move in the game
+current_player = '' # contains the name of the player who has to make the next move in the game
 
 # game board
 states = []
 cells = []
 current_player_display = -1
+playing_board = -1
 
 # adds widgets to the gui
-def add_widgets(window):
+def add_widgets():
     # title display
     title_label = Label(window, text='Tic-Tac-Toe', bg='lightblue', fg='darkblue', relief=RAISED, borderwidth=10, font=('Times', 30, 'bold'))
     title_label.place(anchor=CENTER, relx=.5, rely=.055)
@@ -29,8 +37,6 @@ def add_widgets(window):
     global current_player_display
     current_player_label = Label(window, text='Current Player:', bg='lightblue', fg='darkblue', font=('Arial', 10, 'bold'))
     current_player_label.place(x=15, y=85)
-    current_player_display = Label(window, text=current_player, bg='lightblue', fg='darkblue', font=('Arial', 10))
-    current_player_display.place(x=120, y=85)
 
     # bottom menu frame
     bottom_frame = Frame(window, width=window_width, height=100, bg='darkblue')
@@ -47,6 +53,7 @@ def add_widgets(window):
 
 # creates the gui
 def create_gui():
+    global window
     window = Tk() # creates window object
     window.title('Tic-Tac-Toe')
     window.geometry('500x700') # width x height
@@ -57,13 +64,11 @@ def create_gui():
     global window_width # begin using the global variable
     window_width = window.winfo_width()
 
-    add_widgets(window) 
-    create_board(window) 
-
-    return window
+    add_widgets() 
+    create_board() 
 
 # creates the tic tac toe board
-def create_board(window):
+def create_board():
     # playing board
     playing_board = Frame(window, width=window_width-30, height=150*3, bg='darkblue')
     playing_board.place(in_=window, anchor=CENTER, relx=.5, rely=.5)
@@ -86,8 +91,7 @@ def create_board(window):
 
     for x in range(3):
         for y in range(3):
-            cells[x][y] = Button(playing_board, width=14, height=7, relief=RAISED, borderwidth=5, 
-                                bg='lightblue', fg='darkblue', 
+            cells[x][y] = Button(playing_board, width=14, height=7, relief=RAISED, borderwidth=5, bg='lightblue', fg='darkblue', 
                                 activebackground='lightblue', command=lambda xx=x, yy=y : make_move(xx, yy))
             
             if (x == 0 and y == 0) or (x == 2 and y == 2):
@@ -97,6 +101,9 @@ def create_board(window):
         
 def make_move(xx, yy):
     global current_player # allows access to global variable
+
+    if not begin_game:
+        return
 
     if current_player == player1_name and states[xx][yy] == 0 and stop_game == False:
         cells[xx][yy].config(text="X")
@@ -112,15 +119,57 @@ def make_move(xx, yy):
         
 # starts the game
 def start_game():
-    return
+    global begin_game
+    global current_player
+    global current_player_display
+
+    if begin_game:
+        tkinter.messagebox.showinfo("Start Game", "Game is already started!")
+        return
+
+    begin_game = True
+    num = random.randint(0,1)
+
+    if num == 1:
+        tkinter.messagebox.showinfo("Start Game", "You make the first move!")
+        current_player = player1_name
+    else:
+        tkinter.messagebox.showinfo("Start Game", "You make the second move!")
+        current_player = player2_name
+
+    current_player_display = Label(window, text=current_player, bg='lightblue', fg='darkblue', font=('Arial', 10))
+    current_player_display.place(x=120, y=85)
 
 # restarts the game
 def restart_game():
+    global begin_game
+    begin_game = False
+
+    tkinter.messagebox.showinfo("Restart Game", "Game restarted!")
+
+    global cells
+    global states
+    global playing_board
+
+    cells = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    states = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+
+
     return
 
 # terminates the gui
 def close_gui():
     window.destroy()
 
-window = create_gui()
+create_gui()
 window.mainloop() # infinte loop used to run the application
