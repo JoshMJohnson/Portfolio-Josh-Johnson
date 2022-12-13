bb054c4887cd237de0d3ad2d96d0117798d278bb
JoshMJohnson <joshjohns21@gmail.com>
2022-12-12 21:03:00 -0600

    Created space for a header

--- TicTacToe/Main.py
+++ TicTacToe/Main.py
@@ -10,9 +10,13 @@ player1 = 'X'
 
 # adds widgets to the gui
 def add_widgets(window):
+    # top display
+    title_label = Label(window, text='Tic-Tac-Toe', bg='lightblue', fg='darkblue', relief=RAISED, borderwidth=10, font=('Times', 30, 'bold'))
+    title_label.place(anchor=CENTER, relx=.5, rely=.055)
+
     # bottom menu frame
     bottom_frame = Frame(window, width=window_width, height=100, bg='darkblue')
-    bottom_frame.place(in_=window, anchor=CENTER, relx=.5, rely=.9)
+    bottom_frame.place(in_=window, anchor=CENTER, relx=.5, rely=.92)
 
     # button dimensions
     button_width = 15
@@ -21,13 +25,13 @@ def add_widgets(window):
     # bottom frame buttons
     Button(bottom_frame, text='Start', width=button_width, height=button_height, command=start_game, bg='lightblue', fg='darkblue', activebackground='lightblue').grid(row=0, column=0, padx=30, pady=15)
     Button(bottom_frame, text='Restart', width=button_width, height=button_height, command=restart_game, bg='lightblue', fg='darkblue', activebackground='lightblue').grid(row=0, column=1)
-    Button(bottom_frame, text='Close', width=button_width, height=button_height, command=close_gui, bg='lightblue', fg='darkblue', activebackground='lightblue').grid(row=0, column=2, padx=30, pady=15)
+    Button(bottom_frame, text='Close', width=button_width, height=button_height, command=close_gui, bg='lightblue', fg='darkblue', activebackground='lightblue').grid(row=0, column=2, padx=30)
 
 # creates the gui
 def create_gui():
     window = Tk() # creates window object
     window.title('Tic-Tac-Toe')
-    window.geometry('500x600') # width x height
+    window.geometry('500x700') # width x height
     window.config(bg='lightblue')
     window.resizable(False, False)
     window.update()
@@ -35,7 +39,8 @@ def create_gui():
     global window_width # begin using the global variable
     window_width = window.winfo_width()
 
-    add_widgets(window)
+    add_widgets(window) 
+    create_board(window) 
 
     return window
 
@@ -43,7 +48,7 @@ def create_gui():
 def create_board(window):
     # playing board
     playing_board = Frame(window, width=window_width-30, height=150*3, bg='darkblue')
-    playing_board.place(in_=window, anchor=CENTER, relx=.5, rely=.42)
+    playing_board.place(in_=window, anchor=CENTER, relx=.5, rely=.5)
 
     # grid cells
     cells = [
@@ -66,7 +71,6 @@ def create_board(window):
                 cells[x][y].grid(row=x, column=y, padx=30, pady=30)    
             else:
                 cells[x][y].grid(row=x, column=y)
-
         
 def clicked(r, c):
     return
@@ -84,5 +88,4 @@ def close_gui():
     window.destroy()
 
 window = create_gui()
-create_board(window) # creates playing board
 window.mainloop() # infinte loop used to run the application
