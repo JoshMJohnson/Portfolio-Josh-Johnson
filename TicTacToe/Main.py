f0308c5e6b082fd31b80dc7ee2390e2ae495a0a7
JoshMJohnson <joshjohns21@gmail.com>
2022-12-12 20:02:12 -0600

    Board layout created

--- TicTacToe/Main.py
+++ TicTacToe/Main.py
@@ -5,6 +5,8 @@
 from tkinter import * # used for GUI Interface
 
 window_width = -1 # window width dimension
+stop_game = False
+player1 = 'X'
 
 # adds widgets to the gui
 def add_widgets(window):
@@ -37,6 +39,38 @@ def create_gui():
 
     return window
 
+# creates the tic tac toe board
+def create_board(window):
+    # playing board
+    playing_board = Frame(window, width=window_width-30, height=150*3, bg='darkblue')
+    playing_board.place(in_=window, anchor=CENTER, relx=.5, rely=.42)
+
+    # grid cells
+    cells = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    states = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+
+    for x in range(3):
+        for y in range(3):
+            cells[x][y] = Button(playing_board, width=15, height=7, bg='lightblue', activebackground='lightblue', command=lambda r = x, c = y : clicked(r, c))
+            
+            if (x == 0 and y == 0) or (x == 2 and y == 2):
+                cells[x][y].grid(row=x, column=y, padx=30, pady=30)    
+            else:
+                cells[x][y].grid(row=x, column=y)
+
+        
+def clicked(r, c):
+    return
+
 # starts the game
 def start_game():
     return
@@ -48,16 +82,6 @@ def restart_game():
 # terminates the gui
 def close_gui():
     window.destroy()
-
-# creates the tic tac toe board
-def create_board(window):
-    # playing board
-    playing_board = Frame(window, width=window_width-30, height=150*3, bg='darkblue')
-    playing_board.place(in_=window, x=15, y=15)
-
-    
-
-
 
 window = create_gui()
 create_board(window) # creates playing board
