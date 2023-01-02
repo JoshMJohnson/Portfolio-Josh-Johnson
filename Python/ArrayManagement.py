ffa94251e8ab5537b996a4ca629e06c26da83aa3
JoshMJohnson <joshjohns21@gmail.com>
2023-01-02 00:56:52 -0600

    Element Deletion

--- Python/ArrayManagement.py
+++ Python/ArrayManagement.py
@@ -39,38 +39,53 @@ array21 = ['Vikings', 'Packers', 'Lions', 'Bears']
 # array specifications
 print("Array Specifications:")
 
-size_array = len(array17)
+size_array = len(array17) # returns size of array
 print("len: " + str(size_array))
 
-count_array = array6.count(2)
+count_array = array6.count(2) # returns number of occurences
 print("Count Number: " + str(count_array))
 
-count_array = array19.count("dog")
+count_array = array19.count("dog") # returns number of occurences
 print("Count Text: " + str(count_array), end="\n\n")
 
 # insertion
 print("Element Insertion:")
 
-temp_array = array5
-temp_array.append(10)
+temp_array = array5.copy()
+temp_array.append(10) # inserts element to the end of the array
 print("Append Number: " + str(temp_array))
 
-temp_array = array16
-temp_array.append("!")
+temp_array = array16.copy()
+temp_array.append("!") # inserts element to the end of the array
 print("Append Text: " + str(temp_array))
 
-temp_array = array17
-temp_array.insert(1, "amazing")
+temp_array = array17.copy()
+temp_array.insert(1, "amazing") # inserts element at index given; pushes all elements previously at index given and after
 print("Insert: " + str(temp_array))
 
-temp_array1 = array20
-temp_array2 = array21
-temp_array1.extend(temp_array2)
+temp_array1 = array20.copy()
+temp_array2 = array21.copy()
+temp_array1.extend(temp_array2) # adds all elements from parameter array to the end of working array in order
 print("extend: " + str(temp_array1), end="\n\n")
 
 # deletion
 print("Element Deletion:")
 
+temp_array = array20.copy()
+temp_array.clear() # Removes all elements from the array
+print("Clear: " + str(temp_array))
+
+temp_array = array5.copy()
+temp_array.pop() # removes last element
+print("Pop No Parameter Given: " + str(temp_array))
+
+temp_array = array5.copy()
+temp_array.pop(1) # removes at index given and pulls all elements with a higher index than given parameter index
+print("Pop With Parameter Given: " + str(temp_array))
+
+temp_array = array19.copy()
+temp_array.remove('dog') # removes first occurrence of parameter
+print("Remove: " + str(temp_array), end="\n\n")
 
 # element locating
 print("Element Locating:")
