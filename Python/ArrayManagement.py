108f9f7ec5cff0ef7ae0101a471174fcdbce85a9
JoshMJohnson <joshjohns21@gmail.com>
2023-01-01 23:45:52 -0600

    Array Specifications

--- Python/ArrayManagement.py
+++ Python/ArrayManagement.py
@@ -14,28 +14,34 @@ array2 = [4, 1] # size = 2
 array3 = [-4, 6, 2] # size = 3
 array4 = [0, 84, -3, 40, -21, 21] # size = 6
 array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9] # size = 9
+array6 = [2, 4, 3, 2, 4, 2, 2] # size = 7
 
 # doubles arrays
-array6 = [3.5] # size = 1
-array7 = [5, 3.6] # size = 2
-array8 = [-1.7, 0, 6] # size = 3
-array9 = [6.9, 7.1, -5, 3.7, -9.8] # size = 5
+array7 = [3.5] # size = 1
+array8 = [5, 3.6] # size = 2
+array9 = [-1.7, 0, 6] # size = 3
+array10 = [6.9, 7.1, -5, 3.7, -9.8] # size = 5
 
 # characters arrays
-array10 = [''] # size = 1
-array11 = ['hello'] # size = 1
-array12 = ['Hello World'] # size = 1
-array13 = ['hello world'] # size = 1
-array14 = ['HELLO WORLD'] # size = 1
-array15 = ['hello', 'world'] # size = 2
-array16 = ['hello', 'world', 'of', 'coding', '!'] # size = 5
-array17 = ['hello world of coding!'] # size = 1
+array11 = [''] # size = 1
+array12 = ['hello'] # size = 1
+array13 = ['Hello World'] # size = 1
+array14 = ['hello world'] # size = 1
+array15 = ['HELLO WORLD'] # size = 1
+array16 = ['hello', 'world'] # size = 2
+array17 = ['hello', 'world', 'of', 'coding', '!'] # size = 5
+array18 = ['hello world of coding!'] # size = 1
+array19 = ['dog', 'cat', 'dog', 'dog', 'animal']
 
 # array specifications
-size_array = len(array16)
-print("Size of array: " + str(size_array))
+size_array = len(array17)
+print("len: " + str(size_array))
 
+count_array = array19.count("dog")
+print("Count Text: " + str(count_array))
 
+count_array = array6.count(2)
+print("Count Number: " + str(count_array))
 
 # insertion
 
