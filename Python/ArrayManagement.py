361998c175cdac3f75bd0f6238962e366df9c2ac
JoshMJohnson <joshjohns21@gmail.com>
2023-01-01 23:25:56 -0600

    Create ArrayManagement.py

---
+++ Python/ArrayManagement.py
@@ -0,0 +1,47 @@
+# Array Management:
+#   - Specifications
+#   - Insertion
+#   - Deletion
+#   - Element Locating
+#
+# Created By: Josh Johnson
+
+# list of arrays
+# integers
+array1 = [0] # size = 1
+array2 = [4, 1] # size = 2
+array3 = [-4, 6, 2] # size = 3
+array4 = [0, 84, -3, 40, -21, 21] # size = 6
+array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9] # size = 9
+
+# doubles
+array6 = [3.5] # size = 1
+array7 = [5, 3.6] # size = 1
+array8 = [-1.7, 0, 6] # size = 1
+array9 = [6.9, 7.1, -5, 3.7, -9.8] # size = 1
+
+# characters
+array10 = [''] # size = 1
+array11 = ['hello'] # size = 1
+array12 = ['Hello World'] # size = 1
+array13 = ['hello world'] # size = 1
+array14 = ['HELLO WORLD'] # size = 1
+array15 = [''] # size = 1
+array16 = ['hello', 'world'] # size = 1
+array17 = ['hello', 'world', 'of', 'coding', '!'] # size = 1
+array18 = ['hello world of coding!'] # size = 1
+
+# array specifications
+size_array = len(array5)
+print("Size of array: " + str(size_array))
+
+# insertion
+
+
+# deletion
+
+
+# element locating
+
+
+# char manipulation
