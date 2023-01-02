cf63602b04e836eabb7b475c2cc6f6f17641c3dd
JoshMJohnson <joshjohns21@gmail.com>
2023-01-01 23:51:19 -0600

    Update ArrayManagement.py

--- Python/ArrayManagement.py
+++ Python/ArrayManagement.py
@@ -3,37 +3,40 @@
 #   - Insertion
 #   - Deletion
 #   - Element Locating
+#   - Element Manipulation
 #
 # Created By: Josh Johnson
 
 array0 = [] # empty array; size = 0
 
 # integers arrays
-array1 = [0] # size = 1
-array2 = [4, 1] # size = 2
-array3 = [-4, 6, 2] # size = 3
-array4 = [0, 84, -3, 40, -21, 21] # size = 6
-array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9] # size = 9
-array6 = [2, 4, 3, 2, 4, 2, 2] # size = 7
+array1 = [0] 
+array2 = [4, 1] 
+array3 = [-4, 6, 2] 
+array4 = [0, 84, -3, 40, -21, 21] 
+array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+array6 = [2, 4, 3, 2, 4, 2, 2] 
 
 # doubles arrays
-array7 = [3.5] # size = 1
-array8 = [5, 3.6] # size = 2
-array9 = [-1.7, 0, 6] # size = 3
-array10 = [6.9, 7.1, -5, 3.7, -9.8] # size = 5
+array7 = [3.5]
+array8 = [5, 3.6] 
+array9 = [-1.7, 0, 6]
+array10 = [6.9, 7.1, -5, 3.7, -9.8] 
 
 # characters arrays
-array11 = [''] # size = 1
-array12 = ['hello'] # size = 1
-array13 = ['Hello World'] # size = 1
-array14 = ['hello world'] # size = 1
-array15 = ['HELLO WORLD'] # size = 1
-array16 = ['hello', 'world'] # size = 2
-array17 = ['hello', 'world', 'of', 'coding', '!'] # size = 5
-array18 = ['hello world of coding!'] # size = 1
+array11 = [''] 
+array12 = ['hello']
+array13 = ['Hello World'] 
+array14 = ['hello world'] 
+array15 = ['HELLO WORLD'] 
+array16 = ['hello', 'world'] 
+array17 = ['hello', 'world', 'of', 'coding', '!']
+array18 = ['hello world of coding!']
 array19 = ['dog', 'cat', 'dog', 'dog', 'animal']
 
 # array specifications
+print("Array Specifications:")
+
 size_array = len(array17)
 print("len: " + str(size_array))
 
@@ -41,15 +44,19 @@ count_array = array19.count("dog")
 print("Count Text: " + str(count_array))
 
 count_array = array6.count(2)
-print("Count Number: " + str(count_array))
+print("Count Number: " + str(count_array), end="\n\n")
 
 # insertion
+print("Element Insertion:")
 
 
 # deletion
+print("Element Deletion:")
 
 
 # element locating
+print("Element Locating:")
 
 
-# char manipulation
+# element manipulation
+print("Element Manipulation:")
