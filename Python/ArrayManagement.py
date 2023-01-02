8f75293964ac48b7ab8c1fa22ecc49b904cc26e2
JoshMJohnson <joshjohns21@gmail.com>
2023-01-01 23:32:40 -0600

    Update ArrayManagement.py

--- Python/ArrayManagement.py
+++ Python/ArrayManagement.py
@@ -6,34 +6,36 @@
 #
 # Created By: Josh Johnson
 
-# list of arrays
-# integers
+array0 = [] # empty array; size = 0
+
+# integers arrays
 array1 = [0] # size = 1
 array2 = [4, 1] # size = 2
 array3 = [-4, 6, 2] # size = 3
 array4 = [0, 84, -3, 40, -21, 21] # size = 6
 array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9] # size = 9
 
-# doubles
+# doubles arrays
 array6 = [3.5] # size = 1
-array7 = [5, 3.6] # size = 1
-array8 = [-1.7, 0, 6] # size = 1
-array9 = [6.9, 7.1, -5, 3.7, -9.8] # size = 1
+array7 = [5, 3.6] # size = 2
+array8 = [-1.7, 0, 6] # size = 3
+array9 = [6.9, 7.1, -5, 3.7, -9.8] # size = 5
 
-# characters
+# characters arrays
 array10 = [''] # size = 1
 array11 = ['hello'] # size = 1
 array12 = ['Hello World'] # size = 1
 array13 = ['hello world'] # size = 1
 array14 = ['HELLO WORLD'] # size = 1
-array15 = [''] # size = 1
-array16 = ['hello', 'world'] # size = 1
-array17 = ['hello', 'world', 'of', 'coding', '!'] # size = 1
-array18 = ['hello world of coding!'] # size = 1
+array15 = ['hello', 'world'] # size = 2
+array16 = ['hello', 'world', 'of', 'coding', '!'] # size = 5
+array17 = ['hello world of coding!'] # size = 1
 
 # array specifications
-size_array = len(array5)
+size_array = len(array16)
 print("Size of array: " + str(size_array))
+
+
 
 # insertion
 
