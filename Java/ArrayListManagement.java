1cdac2eea9816bd1f9d552e84d1f3eed0934fd4a
JoshMJohnson <joshjohns21@gmail.com>
2023-01-07 22:43:58 -0600

    Update ArrayListManagement.java

--- Java/ArrayListManagement.java
+++ Java/ArrayListManagement.java
@@ -17,16 +17,16 @@ public class ArrayListManagement {
 
     /* integer lists */
     private ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(0)); 
-    private ArrayList<Integer> list2 = new ArrayList<Integer>() {{add(4); add(1);}};
-    private ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(-4, 6, 2)); 
-    private ArrayList<Integer> list4 = new ArrayList<Integer>(Arrays.asList(0, 84, -3, 40, -21, 21));
+    private ArrayList<Integer> list2 = new ArrayList<>() {{add(4); add(1);}};
+    private ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(-4, 6, 2)); 
+    private ArrayList<Integer> list4 = new ArrayList<>(Arrays.asList(0, 84, -3, 40, -21, 21));
     private ArrayList<Integer> list5 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)); 
     private ArrayList<Integer> list6 = new ArrayList<>(Arrays.asList(2, 4, 3, 2, 4, 2, 2)); 
     private ArrayList<Integer> list7 = new ArrayList<>(Arrays.asList(1, (int) 2.0, 3, 4, (int) 5.2, 6, 7, (int) 8.5, 9, (int) 10.9, 11)); 
 
     /* double lists */
     private ArrayList<Double> list8 = new ArrayList<Double>(Arrays.asList(3.5));
-    private ArrayList<Double> list9 = new ArrayList<Double>(Arrays.asList((double) 5, 3.6)); 
+    private ArrayList<Double> list9 = new ArrayList<>(Arrays.asList((double) 5, 3.6)); 
     private ArrayList<Double> list10 = new ArrayList<>(Arrays.asList(-1.7, (double) 0, (double) 6));
     private ArrayList<Double> list11 = new ArrayList<>(Arrays.asList(6.9, 7.1, (double) -5, 3.7, -9.8)); 
 
@@ -38,10 +38,10 @@ public class ArrayListManagement {
 
     /* string lists */
     private ArrayList<String> list14 = new ArrayList<String>(Arrays.asList("")); 
-    private ArrayList<String> list15 = new ArrayList<String>(Arrays.asList("hello"));
-    private ArrayList<String> list16 = new ArrayList<String>(Arrays.asList("Hello World"));
-    private ArrayList<String> list17 = new ArrayList<String>(Arrays.asList("hello world")); 
-    private ArrayList<String> list18 = new ArrayList<String>(Arrays.asList("HELLO WORLD"));
+    private ArrayList<String> list15 = new ArrayList<>(Arrays.asList("hello"));
+    private ArrayList<String> list16 = new ArrayList<>(Arrays.asList("Hello World"));
+    private ArrayList<String> list17 = new ArrayList<>(Arrays.asList("hello world")); 
+    private ArrayList<String> list18 = new ArrayList<>(Arrays.asList("HELLO WORLD"));
     private ArrayList<String> list19 = new ArrayList<>(Arrays.asList("hello", "world"));
     private ArrayList<String> list20 = new ArrayList<>(Arrays.asList("hello", "world", "of", "coding", "!"));
     private ArrayList<String> list21 = new ArrayList<>(Arrays.asList("hello world of coding!"));
@@ -71,8 +71,12 @@ public class ArrayListManagement {
         System.out.println("--- Array Specifications ---");
 
         /* arraylist methods */
-        System.out.println("List: " + list26.toString()); /* prints contents of list to stdout */
-        System.out.println("Size: " + list5.size());
+        System.out.println("List: " + list26.toString()); /* converts list into a string */
+        System.out.println("Size: " + list5.size()); /* shows number of elements within the list */
+
+        Object[] array = list3.toArray(); /* converts an arraylist to an array */
+        System.out.println("toArray: " + Arrays.toString(array));
+
         System.out.println();
     }
 
@@ -81,9 +85,15 @@ public class ArrayListManagement {
         System.out.println("--- Array Insertion ---");
 
         /* arraylist methods */
-        ArrayList<Integer> tempList = new ArrayList<>(list5);
-        tempList.add(21);
-        System.out.println("Add: " + tempList.toString());
+        ArrayList<Integer> cloneListInt = new ArrayList<>(list5);
+        cloneListInt.add(21); /* appends the specified element to the end of the list */
+        System.out.println("Add: " + cloneListInt.toString());
+
+        ArrayList<Number> cloneListNumber = new ArrayList<>(list12);
+        cloneListNumber.add(6, 5); /* enters a value at a given index; pushing all values previously at and after given index */
+        System.out.println("Add; index: " + cloneListNumber.toString());
+
+
 
         System.out.println();
     }
