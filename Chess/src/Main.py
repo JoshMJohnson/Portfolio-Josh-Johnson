377d312ae2ef45479746fcf426d9af90e3ca4851
JoshMJohnson <joshjohns21@gmail.com>
2023-02-08 21:58:54 -0600

    Project set up

---
+++ Chess/src/Main.py
@@ -0,0 +1,8 @@
+'''
+Loads up a chess board and allows the game of chess to be played
+
+Created By: Josh Johnson
+'''
+
+import time # time access and conversions
+from tkinter import * # used for GUI Interface
