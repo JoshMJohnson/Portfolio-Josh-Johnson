e04d40270be4202d45d236add8342aea5eef4062
JoshMJohnson <joshjohns21@gmail.com>
2023-02-13 22:27:16 -0600

    Set up for multiple different chess sets

--- Chess/Main.py
+++ Chess/Main.py
@@ -20,8 +20,9 @@ BOARD_WIDTH = 400 # game board width
 BOARD_HEIGHT = 400 # game board height
 DIMENSION = 8 # 8x8 board
 TILE_SIZE = BOARD_WIDTH // DIMENSION # size of a square (tile) on the gui
-PIECE_IMAGES = {} # holds the locations of all the chess piece images
 MAX_FPS = 15 # for animations
+PIECE_IMAGES = {} # global dictionary of chess piece images
+chess_set = 1 # indicates which chess set to use; default is set 1
 
 # window settings
 WINDOW_WIDTH = BOARD_WIDTH + 300
@@ -42,25 +43,49 @@ heading_starting_x_coordinate = GAP
 heading_starting_y_coordinate = GAP
 
 '''
-global dictionary of images
+loads the desired chess set
 '''
-def load_images(set):
+def load_chess_set(screen):
+    if chess_set == 1: # chess set 1
+        piece_set = "Set1"
+        background_color = 'light grey'
+        heading_background_color = 'white'
+        font_color = 'black'
+        game_log_background_color = 'white'
+    elif chess_set == 2: # TODO chess set 2
+        piece_set = "Set2"
+        background_color = ''
+        heading_background_color = ''
+        font_color = ''
+        game_log_background_color = ''
+    else: # TODO chess set 3
+        piece_set = "Set3"
+        background_color = ''
+        heading_background_color = ''
+        font_color = ''
+        game_log_background_color = ''
+
+    # * load chess pieces
     base_path = path.dirname(__file__) # finds absolute path for the project
 
-    # determines the piece set
-    piece_set = ""
-    if set == 1:
-        piece_set = "Set1"
-    elif set == 2:
-        piece_set = "Set2"
-    else:
-        piece_set = "Set3"
-
-    # loads the piece set
     pieces = ["black_rook", "black_knight", "black_bishop", "black_queen", "black_king", "black_bishop", "black_knight", "black_rook", "black_pawn",
                 "white_rook", "white_knight", "white_bishop", "white_queen", "white_king", "white_bishop", "white_knight", "white_rook", "white_pawn"]
+    
     for piece in pieces:
         PIECE_IMAGES[piece] = pygame.transform.scale(pygame.image.load(base_path + "/Game_Images/Piece_Sets/" + piece_set + "/" + piece + ".png"), (TILE_SIZE, TILE_SIZE))
+
+    # * loads set background    
+    screen.fill(pygame.Color(background_color))
+
+    # * heading above game board
+    pygame.draw.rect(screen, heading_background_color, pygame.Rect(heading_starting_x_coordinate, heading_starting_y_coordinate, heading_width, heading_height))
+    title_font = pygame.font.SysFont('monospace', 32, 'bold')
+    title_label = title_font.render("The Game of Chess", True, font_color)
+    title_rect = title_label.get_rect(center=(heading_width / 2 + GAP, GAP + 30))
+    screen.blit(title_label, title_rect)
+
+    # * prepare game log panel
+    pygame.draw.rect(screen, game_log_background_color, pygame.Rect(log_frame_starting_x_coordinate, log_frame_starting_y_coordinate, log_frame_width, log_frame_height))
 
 '''
 main function
@@ -71,22 +96,11 @@ def main():
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     clock = pygame.time.Clock()
     pygame.display.set_caption("Lets Play Chess!")
-    screen.fill(pygame.Color("light grey"))
-
-    # * prepare game on load up of program
-    # heading above game board
-    pygame.draw.rect(screen, "white", pygame.Rect(heading_starting_x_coordinate, heading_starting_y_coordinate, heading_width, heading_height))
-    title_font = pygame.font.SysFont('monospace', 32, 'bold')
-    title_label = title_font.render("The Game of Chess", True, 'black')
-    title_rect = title_label.get_rect(center=(heading_width / 2 + GAP, GAP + 30))
-    screen.blit(title_label, title_rect)
-
-    # prepare game log panel
-    pygame.draw.rect(screen, "white", pygame.Rect(log_frame_starting_x_coordinate, log_frame_starting_y_coordinate, log_frame_width, log_frame_height))
-        
+            
     # game board
     game_state = GameState.GameState()
-    load_images(1) # TODO change parameter for each set chosen; 1, 2, or 3
+    load_chess_set(screen) 
+    draw_ranks_files(screen)
 
     tile_selected = () # keeps track of the last tile clicked by the user
     player_clickes = [] # keeps track of a plyaer clicks; two tuples: [(x1,y1), (x2,y2)]
@@ -131,22 +145,29 @@ def main():
 creates all graphics of the game 
 '''
 def draw_game_state(screen, game_state):
-    draw_board(screen) 
+    draw_board_tiles(screen) 
     draw_pieces(screen, game_state.board) 
 
 ''' 
-draws the squares on the board; top left square is always light
+draws the squares on the game board; top left square is always light
 '''
-def draw_board(screen):
-    # chess sets
-    colors = [pygame.Color("white"), pygame.Color("grey")] # set 1
-    # TODO set 2
-    # TODO set 3
+def draw_board_tiles(screen):
+    if chess_set == 1: # chess set 1
+        tile_color1 = 'white'
+        tile_color2 = 'grey'
+    elif chess_set == 2: # TODO chess set 2
+        tile_color1 = ''
+        tile_color2 = ''
+    else: # TODO chess set 3
+        tile_color1 = ''
+        tile_color2 = ''
 
-    # colors the tiles
+    tile_colors = [pygame.Color(tile_color1), pygame.Color(tile_color2)]
+
+    # colors the tiles onto the game board
     for row in range(DIMENSION):
         for col in range(DIMENSION):
-            color = colors[((row + col) % 2)]
+            color = tile_colors[((row + col) % 2)]
             pygame.draw.rect(screen, color, pygame.Rect((col * TILE_SIZE) + 25, (row * TILE_SIZE) + WINDOW_HEIGHT - BOARD_HEIGHT - 25, TILE_SIZE, TILE_SIZE))
 
 '''
@@ -162,9 +183,15 @@ def draw_pieces(screen, board):
                 screen.blit(PIECE_IMAGES[piece], pygame.Rect((col * TILE_SIZE) + 25, (row * TILE_SIZE) + WINDOW_HEIGHT - BOARD_HEIGHT - 25, TILE_SIZE, TILE_SIZE))
 
 '''
+draws the ranks and files of the game board
+'''
+def draw_ranks_files(screen): # TODO 
+    pass
+
+'''
 display game log in panel 
 '''
-def display_game_log(screen): # TODO make scrollable game log
+def display_game_log(screen): # TODO make a scrollable game log
     pass
 
 
